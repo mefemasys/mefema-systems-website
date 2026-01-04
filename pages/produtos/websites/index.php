57a59bc74eef19201fcbc4027a09077e333de906
Meldo Maunze <114779063+mmaunze@users.noreply.github.com>
2026-01-04 10:42:52 +0200

    Add header, hero, and footer includes to index.php

--- pages/produtos/websites/index.php
+++ pages/produtos/websites/index.php
@@ -1,1 +1,5 @@
 
+<?php $pageTitle = "Websites - MEFEMA Systems - Soluções Tecnológicas Inteligentes"; ?>
+<?php include 'includes/header.php'; ?>
+<?php include 'pages/produtos/hero.php'; ?>
+<?php include 'includes/footer.php'; ?>
