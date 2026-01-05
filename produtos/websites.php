c936dc601c02443279233cd4a9b8d83e0521608c
Meldo Maunze <114779063+mmaunze@users.noreply.github.com>
2026-01-05 19:22:39 +0200

    Add includes for product page sections

--- produtos/websites.php
+++ produtos/websites.php
@@ -1,1 +1,6 @@
-
+<?php $pageTitle = "Produtos - MEFEMA Systems - Soluções Tecnológicas Inteligentes"; ?>
+<?php include 'includes/header.php'; ?>
+<?php include 'pages/produtos/hero.php'; ?>
+<?php include 'pages/produtos/lista-produtos.php'; ?>
+<?php include 'pages/produtos/cta.php'; ?>
+<?php include 'includes/footer.php'; ?>
