b327dcb099695c808fa8125c4405cc9699453f74
MEFEMA Systems <dev@mefemasys.co.mz>
2026-01-08 01:38:02 +0200

    Add includes for header, footer, and sections

--- produtos/websites.php
+++ produtos/websites.php
@@ -1,1 +1,8 @@
-
+<?php $pageTitle = "Websites - MEFEMA Systems - Soluções Tecnológicas Inteligentes para Empresas Inteligentes"; ?>
+<?php include '../includes/header.php'; ?>
+<?php include '../pages/inicio/hero.php'; ?>
+<?php include '../pages/inicio/estrategia.php'; ?>
+<?php include '../pages/inicio/servicos.php'; ?>
+<?php include '../pages/inicio/stats.php'; ?>
+<?php include '../pages/inicio/valores.php'; ?>
+<?php include '../includes/footer.php'; ?>
