362c8c7da5d79705ebf282ed2354349ab0df7c2b
MEFEMA Systems <dev@mefemasys.co.mz>
2026-01-08 01:50:40 +0200

    Update websites.php

--- produtos/websites.php
+++ produtos/websites.php
@@ -1,15 +1,12 @@
 <?php 
-// 1. Chama o arquivo de configuração (único lugar que usa o servidor como base)
-require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
+// Chama o arquivo de configuração na nova pasta
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config/path_config.php';
 
-$pageTitle = "Websites - MEFEMA Systems - Soluções Tecnológicas Inteligentes";
+$pageTitle = "Websites - MEFEMA Systems";
 
-// 2. Carrega as partes usando a função que criamos ou ROOT_PATH
+// Agora você usa os caminhos a partir da raiz do site
 get_part('includes/header.php'); 
 get_part('pages/inicio/hero.php'); 
-get_part('pages/inicio/estrategia.php'); 
 get_part('pages/inicio/servicos.php'); 
-get_part('pages/inicio/stats.php'); 
-get_part('pages/inicio/valores.php'); 
 get_part('includes/footer.php'); 
 ?>
