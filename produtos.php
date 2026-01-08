7274e5196d5425c4fa919f97d0798610d40fd337
MEFEMA Systems <dev@mefemasys.co.mz>
2026-01-08 19:55:31 +0200

    Update produtos.php

--- produtos.php
+++ produtos.php
@@ -1,4 +1,4 @@
-<?php $pageTitle = "Produtos - MEFEMA Systems - Soluções Tecnológicas Inteligentes"; ?>
+<?php $pageTitle = "Produtos - MEFEMA Systems - Soluções Tecnológicas Inteligentes Para Empresas Inteligentes "; ?>
 <?php include 'includes/header.php'; ?>
 <link rel="stylesheet" href="/assets/css/produtos.css">
 <?php include 'pages/produtos/hero.php'; ?>
