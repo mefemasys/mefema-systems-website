7b2b7c190f6a71c548f7b5ea1133be266e9e8b10
MEFEMA Systems <dev@mefemasys.co.mz>
2026-01-08 20:10:55 +0200

    Update produtos.php

--- produtos.php
+++ produtos.php
@@ -5,8 +5,6 @@ get_part('includes/header.php');
 ?>
 
 <link rel="stylesheet" href="/assets/css/produtos.css">
-<?php include 'includes/header.php'; ?>
-<link rel="stylesheet" href="/assets/css/produtos.css">
 <?php include 'pages/produtos/hero.php'; ?>
 <?php include 'pages/produtos/lista-produtos.php'; ?>
 <?php include 'pages/produtos/cta.php'; ?>
