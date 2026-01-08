269287c0016ef0e8271410d5a09096f2391fc2db
MEFEMA Systems <dev@mefemasys.co.mz>
2026-01-08 20:10:14 +0200

    Update produtos.php

--- produtos.php
+++ produtos.php
@@ -1,7 +1,13 @@
-<?php $pageTitle = "Produtos - MEFEMA Systems - Soluções Tecnológicas Inteligentes"; ?>
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config/path_config.php';
+$pageTitle = "Produtos - MEFEMA Systems - Soluções Tecnológicas Inteligentes";
+get_part('includes/header.php');
+?>
+
+<link rel="stylesheet" href="/assets/css/produtos.css">
 <?php include 'includes/header.php'; ?>
 <link rel="stylesheet" href="/assets/css/produtos.css">
 <?php include 'pages/produtos/hero.php'; ?>
 <?php include 'pages/produtos/lista-produtos.php'; ?>
 <?php include 'pages/produtos/cta.php'; ?>
-<?php include 'includes/footer.php'; ?>
+<?php get_part('includes/footer.php'); ?>
