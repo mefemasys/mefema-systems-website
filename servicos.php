f04f55c1a5bd2ae01bb485b206f00034fdd1450a
Meldo Maunze <114779063+mmaunze@users.noreply.github.com>
2026-01-09 08:53:13 +0200

    Replace lista_servicos.php with servicos.php

--- servicos.php
+++ servicos.php
@@ -1,6 +1,6 @@
 <?php $pageTitle = "Serviços - MEFEMA Systems - Soluções Tecnológicas Inteligentes"; ?>
 <?php include 'includes/header.php'; ?>
 <?php include 'pages/servicos/hero.php'; ?>
-<?php include 'pages/servicos/lista_servicos.php'; ?>
+<?php include 'pages/inicio/servicos.php'; ?>
 <?php include 'pages/servicos/cta.php'; ?>
 <?php include 'includes/footer.php'; ?>
