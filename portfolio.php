e1e0adb5da43ae15f281af88c9d8904482f2a89e
Meldo Maunze <114779063+mmaunze@users.noreply.github.com>
2026-01-11 14:01:20 +0200

    Update portfolio.php

--- portfolio.php
+++ portfolio.php
@@ -1,4 +1,4 @@
-<?php $pageTitle = "Portfólio - MEFEMA Systems - Trabalhos Realizados"; ?>
+<?php $pageTitle = "Nosso Portfólio - MEFEMA Systems - Soluções Tecnológicas Inteligentes para Empresas Inteligentes"; ?>
 <?php include 'includes/header.php'; ?>
 <?php include 'pages/portfolio/portfolio_data.php'; ?>
 <?php include 'pages/portfolio/hero.php'; ?>
